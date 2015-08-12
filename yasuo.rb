0xsauby/yasuo

commit db150b6eb135f00e3109d55c1975f62bad8afa32

minor bug fixes

yasuo.rb
@@ -128,6 +128,8 @@ private
   def process_nmap_scan
 
     puts "Using nmap scan output file #{@nmap_filename}"
+    @target_urls = []
+    @open_ports = 0
 
     xml = Nmap::XML.new(@nmap_filename)
 
@@ -194,7 +196,7 @@ private
 
       host.each_port do |port|
         open_port = "#{port.state}" == "open"
-        web_service = ("#{port.service}".include?("http") or port.service == "websm" or port.service.ssl?)
+        web_service = ("#{port.service}".include?("http") or port.service == "websm" or "#{port.service}".include?("ssl"))
         wrapped_service = "#{port.service}".include?("tcpwrapped")
 
         next unless open_port and (web_service or wrapped_service)
@@ -205,7 +207,7 @@ private
           schemes = ["https", "http"]
         else
           status = "open"
-          if port.service.ssl?
+          if ("#{port.service}".include?("ssl") or "#{port.service}".include?("https"))
             schemes = ["https"]
           else
             schemes = ["http"]

commit 05faa64e3a5d6bc4278c5f7a24ce71766e970d66

Fix #21, regression introduced by commit db150b6

yasuo.rb
@@ -226,7 +226,7 @@ private
 
       host.each_port do |port|
         open_port = "#{port.state}" == "open"
-        web_service = ("#{port.service}".include?("http") or port.service == "websm" or "#{port.service}".include?("ssl"))
+        web_service = ("#{port.service}".include?("http") or port.service == "websm" or port.service.ssl?)
         wrapped_service = "#{port.service}".include?("tcpwrapped")
 
         next unless open_port and (web_service or wrapped_service)
@@ -237,7 +237,7 @@ private
           schemes = ["https", "http"]
         else
           status = "open"
-          if ("#{port.service}".include?("ssl") or "#{port.service}".include?("https"))
+          if port.service.ssl?
             schemes = ["https"]
           else
             schemes = ["http"]